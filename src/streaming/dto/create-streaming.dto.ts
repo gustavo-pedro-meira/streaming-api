66526648eaa2b9d142e902ac440119ba03288c26
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-09-08 09:51:27 -0300

    Organizando a Validação de Entradas com DTOs e Pipes

--- src/streaming/dto/create-streaming.dto.ts
+++ src/streaming/dto/create-streaming.dto.ts
@@ -1,1 +1,31 @@
-export class CreateStreamingDto {}
+import { StreamingType } from '../entities/streaming-type.entity';
+import {
+  IsNotEmpty,
+  MinLength,
+  IsString,
+  IsEnum,
+  IsDateString,
+} from 'class-validator';
+
+export class CreateStreamingDto {
+  @IsNotEmpty()
+  @MinLength(3)
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsEnum()
+  @IsNotEmpty()
+  type: StreamingType;
+
+  @IsDateString()
+  @IsNotEmpty()
+  releaseYear: Date;
+
+  @MinLength(3)
+  @IsString()
+  @IsNotEmpty(StreamingType)
+  genre: string;
+}
--- src/streaming/dto/update-streaming.dto.ts
+++ src/streaming/dto/update-streaming.dto.ts
@@ -1,4 +1,37 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateStreamingDto } from './create-streaming.dto';
+import { StreamingType } from '../entities/streaming-type.entity';
+import {
+  IsNotEmpty,
+  MinLength,
+  IsString,
+  IsEnum,
+  IsDateString,
+  IsOptional,
+} from 'class-validator';
 
-export class UpdateStreamingDto extends PartialType(CreateStreamingDto) {}
+export class CreateStreamingDto {
+  @IsNotEmpty()
+  @MinLength(3)
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsEnum()
+  @IsNotEmpty()
+  @IsOptional()
+  type?: StreamingType;
+
+  @IsDateString()
+  @IsNotEmpty()
+  @IsOptional()
+  releaseYear?: Date;
+
+  @MinLength(3)
+  @IsString()
+  @IsNotEmpty(StreamingType)
+  @IsOptional()
+  genre?: string;
+}
