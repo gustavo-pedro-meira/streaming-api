e5bc573845a79fe42a700c6e79de008b43c9a4c9
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-09-08 10:29:04 -0300

    Configuração do Banco de Dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StreamingModule } from './streaming/streaming.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [StreamingModule],
+  imports: [
+    StreamingModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'nest-crud',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,5 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
 }
 bootstrap();
